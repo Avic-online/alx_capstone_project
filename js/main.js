1bd72d842aa74cf95f49c4339b545a1d86c819a9
Avic-online <avicdon1@gmail.com>
2024-03-08 11:06:59 +0100

    okay worked on adding event icons
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ const container = document.querySelector('.container');
 const searchForm = document.querySelector('form');
 const searchResultDiv = document.querySelector('.search-result');
 let searchValue = '';
-const app_id = 'ca12959c'
-const app_key = 'c19751ea18825842d6846c57ebfd1898'
+const app_id = 'ca12959c';
+const app_key = 'c19751ea18825842d6846c57ebfd1898';
 
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -35,7 +35,7 @@ function generateHTML(results) {
             <div class="flex-ctn">
                 <a class="view-btn" href="${result.recipe.url}" target="_blank">View Recipe</a>
 
-                <a href="#" title="Mark as favourite"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                <a href="#" title="Mark as favourite" id="fav-btn"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                     <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
                 </svg></a>
 
@@ -52,4 +52,6 @@ function generateHTML(results) {
     })
     searchResultDiv.innerHTML = generatedHTML;
 }
+
+
 

636b1fba61879cdca8a7628a11a31e4153443655
Avic-online <avicdon1@gmail.com>
2024-03-15 08:51:12 +0100

    push for the five star input session
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- js/main.js
+++ js/main.js
@@ -62,11 +62,11 @@ function generateHTML(results) {
             <div class="flex-ctn">
                 <a class="view-btn" href="${result.recipe.url}" target="_blank">View Recipe</a>
 
-                <a href="#" title="Mark as favourite"  onclick="addToFavourites('${result.recipe.label},${result.recipe.url}, ${result.recipe.image}')"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                <a href="#" title="Mark as favourite"  onclick="addToFavourites('${result.recipe.label}')"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                     <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
                 </svg></a>
 
-                <a href="#" title="Kindly rate us!"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-7 h-6">
+                <a href="#" title="Kindly rate us!" id="addRating" onclick="addReview()"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-7 h-6">
                     <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
                 </svg></a>
 
@@ -158,3 +158,15 @@ function DisplayFavourites(Key, displayFav) {
         displayDiv.innerHTML = '<p class="favWhite">No favorites available.</p>';
     }
 }
+
+function addReview(){
+    const add_review = document.querySelector('#addRating');
+    add_review.style.display = 'flex'
+}
+function reviewSubmit() {
+    const review_submit = document.getElementById('#reviewSubmit');
+    alert('thank you for reviewing us!')
+}
+
+
+// <a href="#" title="Mark as favourite"  onclick="addToFavourites('${result.recipe.label},${result.recipe.url}, ${result.recipe.image}')"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6"></svg>
